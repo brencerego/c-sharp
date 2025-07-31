51eb0cc70040bd7d5228f319e2b87e90403d79e5
brencerego <brencerego18@gmail.com>
2025-07-31 15:15:38 +0530

    Add files via upload

---
+++ GreetPerson/GreetPerson/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GreetPerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name:");
+            String name = Console.ReadLine();
+            Greet g = new Greet(name);
+            g.greet();
+        }
+    }
+}
