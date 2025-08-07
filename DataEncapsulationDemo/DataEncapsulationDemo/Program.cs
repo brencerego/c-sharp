6ed7fd9f6203084e594d930a5bd74c7bd042b38a
brencerego <brencerego18@gmail.com>
2025-08-07 14:51:55 +0530

    Add files via upload

---
+++ DataEncapsulationDemo/DataEncapsulationDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using DataEncapsulationDemo;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Brence";
+            person.Age = 18;
+            Console.WriteLine($"The name is {person.Name} and age is {person.Age}");
+
+        }
+    }
+
