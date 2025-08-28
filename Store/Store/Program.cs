234a5ca0ba31774613d6cfec2b85285a96cc9438
brencerego <brencerego18@gmail.com>
2025-08-28 14:22:20 +0530

    Add files via upload

---
+++ Store/Store/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("Blanket", 99.99);
+            p.Display();
+            EProduct e = new EProduct("Washing Machine", 499.99, "Haier", 5);
+            e.Display();
+            Smartphone s = new Smartphone("Smartphone", 50000, "iPhone", 1, "iOS");
+            s.Display();
+        }
+    }
+}
