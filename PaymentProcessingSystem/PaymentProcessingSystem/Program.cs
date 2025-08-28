aec738084d62c45d0dd2631f1db047c2f71e0679
brencerego <brencerego18@gmail.com>
2025-08-28 15:25:19 +0530

    Add files via upload

---
+++ PaymentProcessingSystem/PaymentProcessingSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PaymentProcessingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Payment creditCardPayment = new CreditCardPayment(1500, DateTime.Now, "564987654", "Brence");
+            creditCardPayment.ProcessPayment();
+
+            Payment upiPayment = new UPIPayment(400, DateTime.Now, "axisbank.upi", "Axis Bank");
+            upiPayment.ProcessPayment();
+        }
+    }
+}
+
