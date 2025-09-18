9ca71d65a980d1326c1c3e7ea0a81cfb8956e865
brencerego <brencerego18@gmail.com>
2025-09-18 14:09:50 +0530

    Add files via upload

---
+++ DigitSumCalculator/DigitSumCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DigitSumCalculator calculator = new DigitSumCalculator();
+
+        calculator.ReadNumber();
+        calculator.CalculateAndDisplaySum();
+
+        Console.ReadKey(); 
+    }
+}
