581a4f1a8142ec35a6c190d0a623720cf6a3f6d6
brencerego <brencerego18@gmail.com>
2025-09-18 14:02:02 +0530

    Add files via upload

---
+++ OddEvenCheck/OddEvenCheck/NumberChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OddEvenCheck
+{
+    class NumberChecker
+    {
+        private int number;
+
+        public void ReadNumber()
+        {
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+        public void DisplayResult()
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is Even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is Odd.");
+            }
+        }
+    }
+}
---
+++ OddEvenCheck/OddEvenCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿using OddEvenCheck;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create object of NumberChecker
+        NumberChecker checker = new NumberChecker();
+
+        // Call methods
+        checker.ReadNumber();
+        checker.DisplayResult();
+    }
+}
